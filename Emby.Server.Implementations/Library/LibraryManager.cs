22a789b71ac1accdfa497184c00ad069f6c0cc59
Tobias Kloy <tobias.kloy@werde-volunteer.info>
2025-02-10 23:11:22 +0100

    Fix IsInMixedFolder not being set for Extras in mixed subfolders

--- Emby.Server.Implementations/Library/LibraryManager.cs
+++ Emby.Server.Implementations/Library/LibraryManager.cs
@@ -2676,6 +2676,8 @@ namespace Emby.Server.Implementations.Library
                 if (current.IsDirectory && _namingOptions.AllExtrasTypesFolderNames.ContainsKey(current.Name))
                 {
                     var filesInSubFolder = _fileSystem.GetFiles(current.FullName, null, false, false);
+                    bool subFolderIsMixedFolder = filesInSubFolder.Count() > 1;
+
                     foreach (var file in filesInSubFolder)
                     {
                         if (!_extraResolver.TryGetExtraTypeForOwner(file.FullName, ownerVideoInfo, out var extraType))
@@ -2683,7 +2685,7 @@ namespace Emby.Server.Implementations.Library
                             continue;
                         }
 
-                        var extra = GetExtra(file, extraType.Value);
+                        var extra = GetExtra(file, extraType.Value, subFolderIsMixedFolder);
                         if (extra is not null)
                         {
                             yield return extra;
@@ -2692,7 +2694,7 @@ namespace Emby.Server.Implementations.Library
                 }
                 else if (!current.IsDirectory && _extraResolver.TryGetExtraTypeForOwner(current.FullName, ownerVideoInfo, out var extraType))
                 {
-                    var extra = GetExtra(current, extraType.Value);
+                    var extra = GetExtra(current, extraType.Value, false);
                     if (extra is not null)
                     {
                         yield return extra;
@@ -2700,7 +2702,7 @@ namespace Emby.Server.Implementations.Library
                 }
             }
 
-            BaseItem? GetExtra(FileSystemMetadata file, ExtraType extraType)
+            BaseItem? GetExtra(FileSystemMetadata file, ExtraType extraType, bool isInMixedFolder)
             {
                 var extra = ResolvePath(_fileSystem.GetFileInfo(file.FullName), directoryService, _extraResolver.GetResolversForExtraType(extraType));
                 if (extra is not Video && extra is not Audio)
@@ -2723,6 +2725,7 @@ namespace Emby.Server.Implementations.Library
 
                 extra.ParentId = Guid.Empty;
                 extra.OwnerId = owner.Id;
+                extra.IsInMixedFolder = isInMixedFolder;
                 return extra;
             }
         }
